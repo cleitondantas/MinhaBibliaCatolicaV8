9654883270969dfa523aab252678216796b090a2
cleitondantas <cleiton.dantas@outlook.com>
2015-11-04 11:04:09 -0200

    Novos Icones

--- app/src/main/java/br/com/v8developmentstudio/minhabibliacatolica/MainActivity.java
+++ app/src/main/java/br/com/v8developmentstudio/minhabibliacatolica/MainActivity.java
@@ -51,7 +51,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
         // Instancia as Variaveis
 
         final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
